fb6de5b17a044c4449582c15f0047228b83daf92
LUCAS MERINI FLORES <lucas.flores@alunos.sc.senac.br>
2025-03-13 08:55:16 -0300

    V0.2

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("DOM completamente carregado e analisado!");
+  });
+  
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then((registration) => {
+          console.log('Service Worker registrado com sucesso:', registration);
+        })
+        .catch((error) => {
+          console.log('Falha ao registrar o Service Worker:', error);
+        });
+    });
+  }
+  
